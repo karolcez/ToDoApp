169f1f13c5aa641a093230ad44da30b43ac78875
karolcez1 <karolcezarygorecki@gmail.com>
2025-02-16 20:56:17 +0100

    scss folder structure, learning scss

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,5 +9,18 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <div className="a">
+      <p className="b">
+        test
+      </p>
+    </div>
+    <h1>header 1</h1>
+    <h2>header 2</h2>
+    <h3>header 3</h3>
+    <h4>header 4</h4>
+    <h5>header 5</h5>
+    <h6>header 6</h6>
+    <button className="button button-orange">test</button>
+    <button className="button button-orange .button-large">btn large</button>
   </React.StrictMode>
 );
